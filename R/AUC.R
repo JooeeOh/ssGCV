41f09d20f5db3a7b59878d9d323364b61e34b26f
JooeeOh <oje15118@gmail.com>
2026-03-18 13:52:40 +0900

    Delete R/AUC.R

--- R/AUC.R
+++
@@ -1,7 +0,0 @@
-AUC <- function(score, truth) {
-  n1 <- sum(truth == 1)
-  n0 <- sum(truth == 0)
-  if (n1 == 0 || n0 == 0) return(NA)
-  r <- rank(score, ties.method = "average")
-  (sum(r[truth == 1]) - n1 * (n1 + 1) / 2) / (n1 * n0)
-}
